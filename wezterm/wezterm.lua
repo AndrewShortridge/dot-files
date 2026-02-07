51614e56ead4bdf062bf3d5166ceb2e3bda05c92
andrew <andrew@pop-os.localdomain>
2026-02-07 12:55:18 -0500

    Updating various wezterm configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -35,6 +35,26 @@ config.window_decorations = "RESIZE"
 
 -- Setting the cursor style
 config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 1000
+
+-- Themeing the tab bars
+config.use_fancy_tab_bar = false
+-- config.window_frame = {}
+
+-- Setting the window padding around the terminal
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
+}
+
+-- Setting the animation and fps explicitly to ensure running at best settings
+config.max_fps = 144
+config.animation_fps = 30
+
+-- Explicitly setting the color terminal to ensure proper use
+config.term = "xtrem-256color"
 
 -- Finally, return the configuration to wezterm:
 return config
