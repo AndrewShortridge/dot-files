7a18fa178a282a0833187e70a89d42b69f57db8d
andrew <andrew@pop-os.localdomain>
2026-02-07 13:48:28 -0500

    Updating wezterm keybinds to same tmux keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,8 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+-- Setting the leader key to "CTRL+a"
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
@@ -54,7 +55,40 @@ config.max_fps = 144
 config.animation_fps = 30
 
 -- Explicitly setting the color terminal to ensure proper use
-config.term = "xtrem-256color"
+-- config.term = "xtrem-256color"
+
+-- --------------------- User Defined keybinds section -------------------------
+config.keys = {
+	-- Creating and closing new tabs
+	-- { key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	-- { key = "q", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+	{ key = "t", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+
+	-- Changing between tabs
+	-- { key = "Tab", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) }, -- This conflicts with some app switchers, may need to update
+	-- { key = "Tab", mods = "ALT|SHIFT", action = wezterm.action.ActivateTabRelative(-1) }, -- This conflicts with some app switchers, may need to update
+	{ key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+
+	-- Creating and closing new splits for panes
+	-- { key = "-", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	--   Setting keybinds similar to tmux keybinds
+	{ key = "|", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) }, -- This does not work without shift (need shift to use the "|" character)
+	{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) }, -- This may conflict with some keybinds for other applications, may need to update later
+
+	-- Changing between splits and panes
+	-- { key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
+	-- { key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+	-- { key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
+	-- { key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
+	--   Setting keybinds simlar to tmux keybinds
+	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+}
 
 -- Finally, return the configuration to wezterm:
 return config
